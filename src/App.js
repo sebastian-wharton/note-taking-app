52fc15ed41e1c4016e2689e46df500e3922f3a32
Sebastian Wharton <sebastian.wharton1@gmail.com>
2022-12-27 21:03:31 -0600

    12/27

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Notes from './notes/Notes';
+import AddUserForm from './forms/AddUserForm';
+import EditNoteForm from './forms/EditNoteForm';
 
 const App = () => {
 	const usersData = [
@@ -8,21 +10,57 @@ const App = () => {
 		{ id: 3, name: 'Ben', username: 'benisphere' },
 	];
 
+	const initialFormState = { id: null, name: '', username: '' };
+
+	// Setting state
 	const [users, setUsers] = useState(usersData);
+	const [currentUser, setCurrentUser] = useState(initialFormState);
+	const [editing, setEditing] = useState(false);
+
+	// This function will take a user object as a parameter, and add them to the users array of objects. The ...users code ensures that all the previous users remain in the array.
+
+	// CRUD Operations
+	const addUser = (user) => {
+		user.id = users.length + 1;
+		setUsers([...users, user]);
+	};
+
+	const deleteUser = (id) => {
+		setEditing(false);
+
+		setUsers(users.filter((user) => user.id !== id));
+	};
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false);
+
+		setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+	};
+
+	const editRow = (user) => {
+		setEditing(true);
+
+		setCurrentUser({
+			id: user.id,
+			name: user.name,
+			username: user.username,
+		});
+	};
 
 	return (
 		<div className='container'>
-			<h1>CRUD App with Hooks</h1>
+			<header>CRUD App with Hooks</header>
 			<div className='title'>
 				<h2>Take a Note...</h2>
-				<h2>Recent Notes</h2>
+				<h2 className='h2'>Recent Notes</h2>
 			</div>
-			<div className='flex-row'>
+			<div className='notes'>
 				<div className='new-note'>
-					<h3>Title</h3>
+					<p>Title</p>
 					<hr />
-					<h3>Note Content</h3>
+					<p>Note Content</p>
 				</div>
+				<AddUserForm addUser={addUser} />
 				<Notes users={users} />
 			</div>
 		</div>
--- src/notes/Notes.js
+++ src/notes/Notes.js
@@ -2,36 +2,29 @@ import React from 'react';
 
 const Notes = (props) => (
 	<table>
-		<tbody>
 			{props.users.length > 0 ? (
 				props.users.map((user) => (
-					<div className='grid'>
-						<div
-							className='old-note'
-							key={user.id}
-						>
-							<div className='flex'>
-								<p>{user.name}</p>
-								<hr />
-								<p>{user.username}</p>
+					<div
+						className='note'
+						key={user.id}
+					>
+						<div className='note-content'>
+							<p>{user.name}</p>
+							<hr />
+							<p>{user.username}</p>
 								<div className='buttons'>
-									<button className='muted-button edit'>
-										Edit
-									</button>
-									<button className='button muted-button'>
-										Delete
-									</button>
+									<button>Edit</button>
+									<button>Delete</button>
 								</div>
-							</div>
 						</div>
 					</div>
+					
 				))
 			) : (
 				<tr>
 					<td colSpan={3}>No users</td>
 				</tr>
 			)}
-		</tbody>
 	</table>
 );
 
